a6d6d5dcc4cd80b3f41d110775aecc62d53be5ef
nihala shirin <nihalashirin02@gmail.com>
2025-08-04 13:15:01 +0530

    user.controllers is credated

---
+++ controllers/users.controller.mjs
@@ -0,0 +1,54 @@
+import Users from '../models/User.mjs';
+
+
+export const createUser = async (req, res) => {
+  try {
+    const { name, email, password, phone } = req.body;
+
+    
+    const existingUser = await Users.findOne({
+      $or: [{ email }, { phone }]
+    });
+
+    if (existingUser) {
+      let message = '';
+      if (existingUser.email === email) message = 'Email already exists.';
+      if (existingUser.phone === phone) message = 'Phone number already exists.';
+      if (existingUser.email === email && existingUser.phone === phone) {
+        message = 'Email and phone number already exist.';
+      }
+
+      return res.status(400).json({ message });
+    }
+
+    const newUser = await Users.create({ name, email, password, phone });
+    res.status(201).json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+};
+
+
+export const blockOrUnblockUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { isBlocked } = req.body;
+
+    const updatedUser = await Users.findByIdAndUpdate(
+      id,
+      { isBlocked },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: isBlocked ? "User blocked successfully" : "User unblocked successfully",
+      user: updatedUser
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user", error });
+  }
+};
