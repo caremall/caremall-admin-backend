1df7781e96908376333a1d5122e10e588391dfd0
Nikhildas10 <nikhildas1027@gmail.com>
2025-08-10 21:50:55 +0530

    feat: add razorpay in user checkout

--- controllers/user/orders.controller.mjs
+++ controllers/user/orders.controller.mjs
@@ -2,10 +2,10 @@ import Razorpay from 'razorpay';
 import Order from '../../models/Order.mjs'
 import crypto from "crypto";
 
-// const razorpay = new Razorpay({
-//   key_id: process.env.RAZORPAY_KEY_ID,
-//   key_secret: process.env.RAZORPAY_KEY_SECRET,
-// });
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
 export const createOrder = async (req, res) => {
     const { items, shippingAddress, paymentMethod, totalAmount } = req.body;
@@ -22,11 +22,11 @@ export const createOrder = async (req, res) => {
       totalPrice: item.totalPrice,
     }));
 
-    // const razorpayOrder = await razorpay.orders.create({
-    //   amount: totalAmount * 100, // in paise
-    //   currency: "INR",
-    //   receipt: `order_rcptid_${Date.now()}`,
-    // });
+    const razorpayOrder = await razorpay.orders.create({
+      amount: totalAmount * 100, // in paise
+      currency: "INR",
+      receipt: `order_rcptid_${Date.now()}`,
+    });
 
     const order = await Order.create({
       user: req.user._id,
@@ -35,13 +35,13 @@ export const createOrder = async (req, res) => {
       paymentMethod,
       paymentStatus: "pending",
       totalAmount,
-    //   razorpayOrderId: razorpayOrder.id,
+      razorpayOrderId: razorpayOrder.id,
     });
 
     res.status(201).json({
       success: true,
       order,
-    //   razorpayOrder,
+      razorpayOrder,
     });
   
 };
