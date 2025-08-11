8c217498c768464ce9fceafc693c1458ff643606
caremall <caremallapp@gmail.com>
2025-08-11 09:48:41 +0530

    Update allowedOrigin.mjs
    test frontend url added in cors options

--- config/cors/allowedOrigin.mjs
+++ config/cors/allowedOrigin.mjs
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:5173', 'http://admin.caremall.in', 'https://www.admin.caremall.in', 'https://admin.caremall.in'];
+const allowedOrigins = ['http://localhost:5173', 'http://admin.caremall.in', 'https://www.admin.caremall.in', 'https://admin.caremall.in','https://caremall-three.vercel.app'];
 
 export default allowedOrigins
