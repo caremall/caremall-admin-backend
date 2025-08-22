ce598993e8edb0d23304097c3937d4138c18ef6c
minhajzein <minhajzein@gmail.com>
2025-08-22 10:50:39 +0530

    cors error resolved

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ const app = express();
 configDotenv();
 connectDB(process.env.DATABASE_URI);
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
