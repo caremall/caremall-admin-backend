eb42c9329114ab4366613fa3671d8bee438125d9
Nikhildas10 <nikhildas1027@gmail.com>
2025-08-28 09:03:13 +0530

    fix: multiple delete routes

--- routes/admin/warehouse.router.mjs
+++ routes/admin/warehouse.router.mjs
@@ -13,8 +13,8 @@ const adminWarehouseRouter = Router();
 adminWarehouseRouter.post("/", createWarehouse);
 adminWarehouseRouter.get("/", getWarehouses);
 adminWarehouseRouter.get("/:id", getWarehouseById);
+adminWarehouseRouter.put("/delete-multiple",deleteWarehouses)
 adminWarehouseRouter.put("/:id", updateWarehouse);
 adminWarehouseRouter.delete("/:id", deleteWarehouse);
-adminWarehouseRouter.put("/delete-multiple",deleteWarehouses)
 
 export default adminWarehouseRouter;
