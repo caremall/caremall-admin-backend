9f4a3e16e53bcb0d545e756639ef6fa6aeedf229
Nikhildas10 <nikhildas1027@gmail.com>
2025-08-31 12:49:06 +0530

    feat: Update authentication routes to use verifyUserAccessToken for enhanced security

--- routes/user/auth.router.mjs
+++ routes/user/auth.router.mjs
@@ -11,14 +11,14 @@ import {
   signup,
 } from "../../controllers/user/auth.controller.mjs";
 import { catchAsyncErrors } from "../../utils/catchAsyncErrors.mjs";
-import { verifyToken } from "../../middlewares/verifyToken.mjs";
+import { verifyUserAccessToken } from "../../utils/generateTokens.mjs";
 
 const router = Router();
 
 router.post("/signup", catchAsyncErrors(signup));
 router.post("/login", login);
-router.get("/me", verifyToken, getLoggedInUserDetails);
-router.put("/edit-profile", verifyToken, editProfile);
+router.get("/me", verifyUserAccessToken, getLoggedInUserDetails);
+router.put("/edit-profile", verifyUserAccessToken, editProfile);
 router.post("/send-otp", sendOtp);
 router.post("/login-otp", loginWithOtp);
 router.post("/refresh-token", refreshAccessToken);
@@ -26,6 +26,6 @@ router.post("/logout", logout);
 router.get("/test", (req, res) => {
   res.status(200).json({ message: "Auth route is working" });
 });
-router.delete("/delete-account", verifyToken, deleteAccount);
+router.delete("/delete-account", verifyUserAccessToken, deleteAccount);
 
 export default router;
