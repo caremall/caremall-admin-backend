a2bff9a09fbcbb21d452e2d111d3b0d727c62483
Nikhildas10 <nikhildas1027@gmail.com>
2025-09-04 21:43:47 +0530

    refactor: improve readability of getAllCategories by formatting populate calls

--- controllers/admin/category.controller.mjs
+++ controllers/admin/category.controller.mjs
@@ -77,8 +77,16 @@ export const getAllCategories = async (req, res) => {
     }
 
     const categories = await Category.find(filter)
-      .populate("products").populate("subcategories")
-      .sort({ createdAt: -1 });
+         .populate({
+           path:"products",
+           select: "productName urlSlug",
+         })
+         .populate({
+           path: "subcategories",
+           select: "name categoryCode",
+         })
+         .sort({ createdAt: -1 });
+   
 
     res.status(200).json(categories);
   } catch (err) {
