b88a7cbd318d1176dd6afebfae9775ea401e74b5
Nikhildas10 <nikhildas1027@gmail.com>
2025-09-05 20:02:18 +0530

    fix: error code in brand delete

--- controllers/admin/brands.controller.mjs
+++ controllers/admin/brands.controller.mjs
@@ -142,7 +142,7 @@ export const deleteBrand = async (req, res) => {
   try {
     const product = await Product.findOne({ brand: req.params.id });
     if (product)
-      return res.status(200).json({
+      return res.status(400).json({
         message: "Brand already used in products",
       });
 
