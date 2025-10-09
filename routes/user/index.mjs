2a8ba40e4110f77964c1f1b80b369b5f0b4229de
abhayk012 <abhaykrishnan012@gmail.com>
2025-10-09 12:22:36 +0530

    added user side testimonial get

--- routes/user/index.mjs
+++ routes/user/index.mjs
@@ -17,7 +17,7 @@ import highlightsRouter from "./highlights.router.mjs";
 import offerCardRouter from "./offer.card.routes.mjs";
 import photoGalleryRouter from "./photo.gallery.router.mjs";
 import productCardRouter from "./product.card.router.mjs";
-import testimonialRouter from ".testimonial.router.mjs";
+import testimonialRouter from "./testimonial.router.mjs";
 // import firstOrderRouter from "./firstorder.router.mjs";
 const userRouter = Router()
 
