2b7f7c9fef1b4a6a26b15fc3a1bc56cb015cf3a0
Raz3-33 <raziq@ocelots.in>
2025-10-29 11:51:14 +0530

    DELETE PRODUCT ISSUE SOLVED

--- routes/warehouse/products.routes.mjs
+++ routes/warehouse/products.routes.mjs
@@ -6,30 +6,26 @@ import {
   getProductBySlug,
   updateProduct,
   getSearchSuggestions,
-  getProductWithInventory
+  getProductWithInventory,
 } from "../../controllers/warehouse/products.controller.mjs";
 import { catchAsyncErrors } from "../../utils/catchAsyncErrors.mjs";
 
 const router = Router();
 
-
 router.get("/search", getSearchSuggestions);
 
 router.route("/").get(getAllProducts).post(catchAsyncErrors(createProduct));
 
-router
-  .route("/get-by-slug/:slug")
-  .get(getProductBySlug)
+router.route("/get-by-slug/:slug").get(getProductBySlug);
 
-
-router
-  .route("/get-by-id/:id")
-  .get(getProductWithInventory)
+router.route("/get-by-id/:id").get(getProductWithInventory);
 
 router.get("/inventory-detail/:productId/:variantId", getProductWithInventory);
 
 router.put("/:id", updateProduct);
 
 router.get("/inventory-detail/:productId", getProductWithInventory);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
